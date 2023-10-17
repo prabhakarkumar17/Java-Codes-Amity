fffef902190b281b6a29e60c7401eb2828cb98cc
Kumarshreyam <147642679+Kumarshreyam@users.noreply.github.com>
2023-10-17 20:36:54 +0530

    Add files via upload
    comparison of two numbers

---
+++ Comparison.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class  Comparison
+{
+  public static void main (String[] args)
+    {
+	 Scanner sc = new Scanner(System.in);
+           System.out.println("Enter the number");
+         int a =sc.nextInt();
+         System.out.println("Enter the second number");
+         int b =sc.nextInt();
+       if (a>b)
+     {
+     	System.out.println("a is  than greater than b");
+      }
+  	else
+      {
+        System.out.println("b is greater than a");
+      }
+   }
+}
+
+
+       
+
