adc529f44fd653f243f40683832db5c4abf2a1f0
ashish-197 <147627686+ashish-197@users.noreply.github.com>
2023-10-17 22:59:17 +0530

    Update Largestof2Number.java

--- Largestof2Number.java
+++ Largestof2Number.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 class Largestof2Number{
     public static void main(String[] args){
-        Scanner sc = new Scanner (Systemin);
+        Scanner sc = new Scanner (System.in);
         int a,b;
-        if(a<=b){
-            System.out.println(" a>b " );
+        System.out.println("Enter first number");
+        a=sc.nextInt();
+        System.out.println("Enter second number");
+        b=sc.nextInt();
+        if(a<b){
+            System.out.println(" a is smaller than b " );
         }
         else{
-            System.out.println(" a<b ");
+            System.out.println(" a is greater than b ");
         }
     }
 }
