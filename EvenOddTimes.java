49416f686798e21366ef6cc98032b2470e9ed0a8
ashish-197 <147627686+ashish-197@users.noreply.github.com>
2023-10-17 22:08:09 +0530

    Add files via upload
    Sir Rectangle file consists of 3 files 1. Area of circle   2.Average of 3 numbers   3. Area of rectangle
    Largestof2Number ka output nhi mila

---
+++ EvenOddTimes.java
@@ -0,0 +1,13 @@
+class EvenOddTimes{
+    public static void main(String[]args){
+        int n=5;
+        if(n%2==0){
+            n=(n*3);
+            System.out.println("The number is even & multiply by 3 = " +n);
+        }
+        else{
+            n=(n*2);
+             System.out.println("The number is Odd & multiply by 2 = "+n);
+        }
+    }
+}
---
+++ Largestof2Number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Largestof2Number{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (Systemin);
+        int a,b;
+        if(a<=b){
+            System.out.println(" a>b " );
+        }
+        else{
+            System.out.println(" a<b ");
+        }
+    }
+}
