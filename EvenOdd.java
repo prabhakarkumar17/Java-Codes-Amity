860a7c138588dad3f4b89d25ccdec5218161c87d
ritikhzb <115146696+ritikhzb@users.noreply.github.com>
2023-10-17 22:03:36 +0530

    Add files via upload
    to check whether the number is odd or even

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd {
+    public static void main(String[] args){
+        int a=7;
+        if (a%2==0)
+        {
+        System.out.println("The Number is Even");
+    }
+    else {
+        System.out.println("The Number is Odd");
+    }
+}
+}
