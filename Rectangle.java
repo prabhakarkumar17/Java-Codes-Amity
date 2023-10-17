75a1c571c39a7d4e745574e7b31afedf7b401fdc
ashish-197 <147627686+ashish-197@users.noreply.github.com>
2023-10-17 22:09:26 +0530

    Add files via upload
    Rectangle file

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+/* class Area {
+    public static void main ( String[] args){
+        double area;
+         double pi=3.14;
+        double r=7;
+         area = ( pi*r*r );
+        System.out.println("area of circle = " + area);
+
+    }
+
+}*/
+
+/*class Average {
+    public static void main (String[] args){
+        long average;
+        int a=1;
+        int b=2;
+        int c=3;
+        avrage =(a+b+c)/3;
+        System.out.println("average of three numbres = " +average);
+    }
+} */
+
+class Rectangle{
+    public static void main(String[] args){
+        double areaofrectangle;
+        double l=5;
+        double b=4;
+       areaofrectangle=l*b;
+       System.out.println("area of rectangle =" + areaofrectangle); 
+    }
+}
