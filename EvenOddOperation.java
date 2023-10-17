dd99df2036c7eb5cfd61f6e9887c38ca4236e7d3
ritikhzb <115146696+ritikhzb@users.noreply.github.com>
2023-10-17 22:05:07 +0530

    Add files via upload
    if the number is even , multiply it by 3
    if the number is odd , multiply it by 2

---
+++ EvenOddOperation.java
@@ -0,0 +1,14 @@
+class EvenOddOperation {
+    public static void main(String[] args){
+        int a=7;
+        if (a%2==0)
+        {
+            a=a*3;
+        System.out.println("The number is even . So after Multipliction by 3 ,The Number is "+a);
+    }
+    else {
+        a=a*2;
+        System.out.println("The number is odd . So after Multipliction by 2 ,The Number is "+a);
+    }
+}
+}
