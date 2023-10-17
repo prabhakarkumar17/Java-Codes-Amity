a5e432fed8802f97acabf9eee863f745e3a85ece
Prabhakar Kumar <42499823+prabhakarkumar17@users.noreply.github.com>
2023-10-17 13:13:19 +0530

    Add files via upload

---
+++ Fruits.java
@@ -0,0 +1,17 @@
+class Fruits {
+    public static void main(String []args)
+    { 
+        if (args.length>0)
+	{
+        	for(int i=0;i<args.length;i++)
+        		System.out.println(args[i]);
+	}
+	else
+        {
+		System.out.println("array is empty");
+        }                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+    }
+
+}
+
