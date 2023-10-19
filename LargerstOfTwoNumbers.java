3773f37c1966d6fd2a2c15e0fe6a1c0c6d9a6bdc
Prabhakar Kumar <42499823+prabhakarkumar17@users.noreply.github.com>
2023-10-19 14:25:04 +0530

    Update LargerstOfTwoNumbers.java

--- LargerstOfTwoNumbers.java
+++ LargerstOfTwoNumbers.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-class LargerstOfTwoNumbers {
+class largest {
  public static void main(String[] args){
      System.out.print("Enter 1st No.: ");
 	Scanner sc= new Scanner(System.in);
 	int a=sc.nextInt();
 	System.out.print("Enter 2nd No.: ");
 	int b=sc.nextInt();
+
 	if (a>b){
 	System.out.print("1st Number is greater");
 	}
-	if (a<b){
-	    System.out.print("2nd Number is greater");
-	}
-	else {
-	    System.out.print("Both Numbers are equal");
+	else if(b>a) {
+	    System.out.print("2nd Numbersis greater");
 	    
-	}
+	} else {
+        System.out.println("Both numbers are equal");
+    }
  }
 }
