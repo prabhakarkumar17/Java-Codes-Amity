4bf91e2af7bb60edaac1fcaa30b7e3aa6bd564aa
ritikhzb <115146696+ritikhzb@users.noreply.github.com>
2023-11-27 17:51:20 +0530

    Add files via upload
    rectangular pattern

---
+++ RectangularPattern.java
@@ -0,0 +1,20 @@
+         import java.util.Scanner;
+
+public class RectangularPattern {
+ public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the no of rows : ");
+    int a = sc.nextInt();
+    
+    System.out.print("Enter the no of columns : ");
+    int b = sc.nextInt();
+
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=b; j++ ){
+            System.out.print("*");
+        }
+        System.out.println(); 
+    }
+ }   
+}
